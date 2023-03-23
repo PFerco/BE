2a8e1f064f630e768d4bbce6758e52685f4ce002
PFerco <ferco_1@hotmail.com>
2023-03-22 19:44:37 -0500

    agregue gunicorn

---
+++ dtos/producto_dto.py
@@ -0,0 +1,18 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
+from models.producto_model import Producto
+from dtos.categoria_dto import CategoriaDto
+
+class ProductoDto(SQLAlchemyAutoSchema):
+    class Meta:
+        # sirve para indicar a mi DTO que ahora queremos q tbn reconozca las columnas q sean fk
+        include_fk = True
+        model = Producto
+
+class MostrarProductoDto(SQLAlchemyAutoSchema):
+    categoria = fields.Nested(nested = CategoriaDto)
+  
+    class Meta:
+        include_fk = True
+        load_instance = True
+        model = Producto
