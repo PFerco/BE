3d8708117dead27650caa0e5d2ecb6774e44aed6
PFerco <ferco_1@hotmail.com>
2023-03-22 23:07:56 -0500

    django

---
+++ reservas/gestion/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    nombre = models.TextField(null=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        # sirve para modificar alguna configuracion de la tabla en nuestra bd
+        db_table = 'categorias'
